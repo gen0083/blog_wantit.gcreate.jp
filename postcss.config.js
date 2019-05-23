d5a40497a9673632bc1ad105bbd9fdae3fbf3ab0
Gen <archiherewego@gmail.com>
2019-05-23 13:21:24 +0900

    Minify css by post-css

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
 module.exports = {
     plugins: [
+        require('postcss-import')(),
         require('autoprefixer')({
             "browsers": [
                 "last 2 versions"
             ]
+        }),
+        require('cssnano')({
+            autoprefixer: false
         })
     ]
 };
