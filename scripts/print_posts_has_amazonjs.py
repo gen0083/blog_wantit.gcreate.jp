ce3e4f4c999c9a4162da58c7d9addb57317b44a3
Gen <archiherewego@gmail.com>
2019-07-04 17:06:24 +0900

    Add script to print posts that has amazonjs blocks
    機械的に書き換えることを考えたが、それをやろうとするとおそらくAmazonのProduct Advertise APIを使う必要がある
    そのため、面倒だが手動で各商品ページに移動してアソシエイトリンクを生成、該当箇所を書き換える戦法をとることにする

---
+++ scripts/print_posts_has_amazonjs.py
@@ -0,0 +1,76 @@
+import os
+import re
+import sys
+import termios
+
+stack = []
+# refer: https://qiita.com/tortuepin/items/9ede6ca603ddc74f91ba
+# 標準入力のファイルディスクリプタを取得
+fd = sys.stdin.fileno()
+
+# fdの端末属性をゲットする
+# oldとnewには同じものが入る。
+# newに変更を加えて、適応する
+# oldは、後で元に戻すため
+old = termios.tcgetattr(fd)
+new = termios.tcgetattr(fd)
+
+# new[3]はlflags
+# ICANON(カノニカルモードのフラグ)を外す
+new[3] &= ~termios.ICANON
+# ECHO(入力された文字を表示するか否かのフラグ)を外す
+new[3] &= ~termios.ECHO
+
+
+def collect_md_files(file):
+    if os.path.isdir(file):
+        files_in_dir = os.scandir(file)
+        for f in files_in_dir:
+            collect_md_files(f)
+    else:
+        base, ext = os.path.splitext(file)
+        if ext == ".md":
+            match = filter_has_amazonjs_block(file)
+            if len(match) > 0:
+                stack.append(dict(path=file.path, amazon=match))
+
+
+def filter_has_amazonjs_block(file):
+    with open(file, mode='r') as f:
+        src = f.read()
+        match = re.findall(r'.*<div data-role="amazonjs".+', src)
+        f.close()
+        return match
+
+
+def user_input_without_enter(count):
+    print(f"remain {count} posts: if exit scripts: enter [q] / print next enter [any]:")
+    try:
+        termios.tcsetattr(fd, termios.TCSANOW, new)
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSANOW, old)
+    return ch
+
+
+# collect target files
+base_path = os.getcwd()
+file_list = os.scandir(os.path.join(base_path, "content", "post"))
+for file in file_list:
+    collect_md_files(file)
+
+count = len(stack)
+print(f"matched posts are {count}")
+for t in stack:
+    print(t["path"])
+    for amazon in t["amazon"]:
+        print(amazon)
+    print("")
+    count -= 1
+    if count == 0:
+        exit(0)
+    # judge to continue or exit by user
+    users_input = user_input_without_enter(count)
+    if users_input.strip() == "q":
+        print("aborted!")
+        exit(0)
